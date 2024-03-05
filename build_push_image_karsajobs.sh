274e958d10d5263ae8012ecc6acd9d79c3b5d1d0
Cydnirn <seno@siotics.org>
2024-03-05 15:18:18 +0700

    feat: Add script

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Set variable for Github token
+CR_PAT=$1
+Hash=$(git rev-parse --short HEAD)
+
+# Build docker image
+docker build --network=host -t karsajobs:$Hash .
+docker image tag karsajobs:$Hash ghcr.io/cydnirn/karsajobs:latest 
+echo $CR_PAT | docker login ghcr.io -u Cydnirn --password-stdin
+docker push ghcr.io/cydnirn/karsajobs:latest 
